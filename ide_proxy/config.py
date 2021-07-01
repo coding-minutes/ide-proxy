5e1ba6f10be7548a62033792d08b31503422d599
Tarun Luthra <tarunluthra987@gmail.com>
2021-07-01 17:16:16 +0530

    Basic api setup

---
+++ ide_proxy/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config:
+    DEBUG = os.environ.get("DEBUG") in ["True", "true", "1", 1]
+    SECRET_KEY = os.environ.get("SECRET_KEY", "abc")
+    ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+    
